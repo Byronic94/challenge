ebf187167229362f72ad50c62b3d3cfc3c38e9a4
Byronic94 <yanyan090123@gmail.com>
2015-08-06 17:35:17 +0800

    change the view of initial page

--- js/initial.js
+++ js/initial.js
@@ -1,10 +1,3 @@
 $(document).ready(function(){
-  $("#sub").click(function(){
-    if($("#challengeForm").is(":hidden")){
-  	    $("#challengeForm").show();
-        
-    } else {
-      $("#challengeForm").submit();
-    }
-  });
+ s
 });
